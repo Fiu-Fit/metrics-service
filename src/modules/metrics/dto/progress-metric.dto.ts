1fda2f18b08e5239e73ec40cdeda62af950b5f77
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-06 17:38:03 -0300

    Created progress metric DTO

---
+++ src/modules/metrics/dto/progress-metric.dto.ts
@@ -0,0 +1,19 @@
+import { Unit } from '@prisma/client';
+import { IsEnum, IsNumber, IsOptional } from 'class-validator';
+
+export class ProgressMetricDTO {
+  @IsNumber()
+  calories_burnt: number;
+
+  @IsNumber()
+  value: number;
+
+  @IsEnum(Unit)
+  unit: Unit;
+
+  @IsNumber()
+  userId: number;
+
+  @IsOptional()
+  updatedAt?: Date;
+}
