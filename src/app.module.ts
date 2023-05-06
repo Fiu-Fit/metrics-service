9bf6facb7f36cd20ec73fdcc4b24af776042c496
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-06 14:43:25 -0300

    Created metrics module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MetricsModule } from './modules/metrics/metrics.module';
 
 @Module({
-  imports:     [ConfigModule.forRoot()],
+  imports:     [ConfigModule.forRoot(), MetricsModule],
   controllers: [AppController],
   providers:   [AppService],
 })
