4fbfdbe5deb95e2cc105a8cd3c16dff4e302ba21
Rocío <rplatini@fi.uba.ar>
2023-05-09 00:37:47 -0300

    Register Goal Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GoalModule } from './modules/goals/goal.module';
 import { MetricsModule } from './modules/metrics/metrics.module';
 
 @Module({
-  imports:     [ConfigModule.forRoot(), MetricsModule],
+  imports:     [ConfigModule.forRoot(), MetricsModule, GoalModule],
   controllers: [AppController],
   providers:   [AppService],
 })
